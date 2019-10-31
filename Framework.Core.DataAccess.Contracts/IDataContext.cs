777cca7f76220a02a0e40af2501050ea7ef7b97b
Alejandro Lijoi <alejandrolijoi@live.com>
2019-10-31 02:04:17 -0300

    Created initial

---
+++ Framework.Core.DataAccess.Contracts/IDataContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Framework.Core.DataAccess.Contracts
+{
+    public interface IDataContext
+    {
+        DbSet<AdminEntity> Admins { get; set; }
+
+        DbSet<BookingEntity> Bookings { get; set; }
+
+        DbSet<OfficeEntity> Offices { get; set; }
+
+        DbSet<Office2RoomsEntity> Office2Rooms { get; set; }
+
+        DbSet<RoomEntity> Rooms { get; set; }
+
+        DbSet<Room2ServiceEntity> Room2Services { get; set; }
+
+        DbSet<UserEntity> Users { get; set; }
+
+        DbSet<ServiceEntity> Services { get; set; }
+
+
+
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        DatabaseFacade Database { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
+
+        void RemoveRange(IEnumerable<object> entity);
+
+        EntityEntry Update(object entity);
+
+    }
+}
